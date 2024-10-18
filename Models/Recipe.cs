2a5217c9b2df8fb90a87853b8a37546605de4137
Raima Shahzad <raimach908@gmail.com>
2024-10-18 14:26:17 +0500

    Add project files.

---
+++ Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿namespace RecipeManagementApp.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public List<string> Ingredients { get; set; }
+        public string? Instructions { get; set; }
+        public string? Category { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public string? Description { get; set; }
+
+    }
+}
